4ae948b5200522deb1d60205f8b1cb17f177987e
yazoma1 <yazoma1@5CD821551X.thn.local>
2019-09-06 10:09:44 +0200

    start

---
+++ prog1Omari/src/prog1Omari/Test1.java
@@ -0,0 +1,17 @@
+package prog1Omari;
+
+public class Test1 {
+
+	public static void main(String [] arg) {
+		
+		double y;  //deklarerar variabel Y
+		y = 100;  // tilldelar  1 till variablen Y
+		
+		
+		System.out.println(y);
+		
+		
+	}
+	
+	
+}
